46c919f9767e5743ccf59f19899c449ae705e6cc
Christopher Armstrong <radix@twistedmatrix.com>
2016-11-26 22:09:23 -0600

    support different-sized planets, get rid of many hardcoded constants

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,16 @@ use ncollide::query;
 use ncollide::shape::Ball;
 use na::{Isometry2, Vector2};
 
+const SHIP_SIZE: f64 = 50.0;
+const SPEED: f64 = 5.0;
+const JUMP_SPEED: f64 = 3.0;
+const AIR_CONTROL_MOD: f64 = 0.1;
+
 #[derive(Debug)]
 struct Planet {
     x: f64,
     y: f64,
+    radius: f64,
 }
 
 #[derive(Debug)]
@@ -29,8 +35,6 @@ pub struct App {
     planets: Vec<Planet>,
     attached_planet: usize
 }
-
-const ATTACHED_HEIGHT: f64 = 75.0;
 
 impl App {
     fn ship_position(&self) -> (f64, f64) {
@@ -48,8 +52,7 @@ impl App {
         const BLUE:  [f32; 4] = [0.0, 0.0, 1.0, 1.0];
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let circle = ellipse::circle(0.0, 0.0, 50.0);
+        let square = rectangle::square(0.0, 0.0, SHIP_SIZE);
 
         let (x, y) = self.ship_position();
         gl.draw(args.viewport(), |c, gl| {
@@ -60,33 +63,33 @@ impl App {
                                        .trans(-25.0, -25.0);
             rectangle(RED, square, transform, gl);
             for planet in &self.planets {
+                let circle = ellipse::circle(0.0, 0.0, planet.radius);
                 ellipse(BLUE, circle, c.transform.trans(planet.x, planet.y), gl);
             }
         });
     }
 
     fn update(&mut self, args: &UpdateArgs, up: bool, _down: bool, left: bool, right: bool) {
-        const SPEED: f64 = 5.0;
         if !self.jumping {
             if left { self.rotation -= SPEED * args.dt; }
             if right { self.rotation += SPEED * args.dt; }
             if up { self.jumping = true; }
         }
         if self.jumping {
-            self.height += SPEED;
-            if left { self.rotation -= SPEED / 10.0 * args.dt}
-            if right { self.rotation += SPEED / 10.0 * args.dt}
+            self.height += JUMP_SPEED;
+            if left { self.rotation -= SPEED * AIR_CONTROL_MOD * args.dt}
+            if right { self.rotation += SPEED * AIR_CONTROL_MOD * args.dt}
         }
 
         // create ship ball
-        let ship_ball = Ball::new(25.0);
+        let ship_ball = Ball::new(SHIP_SIZE);
         let (ship_x, ship_y) = self.ship_position();
         let ship_pos = Isometry2::new(Vector2::new(ship_x, ship_y), na::zero());
         for (planet_index, planet) in (&self.planets).iter().enumerate() {
             if planet_index == self.attached_planet {
                 continue;
             }
-            let planet_ball = Ball::new(50.0);
+            let planet_ball = Ball::new(planet.radius);
             let planet_pos = Isometry2::new(
                 Vector2::new(planet.x, planet.y),
                 na::zero());
@@ -95,7 +98,7 @@ impl App {
                 // We are landing on a new planet
                 self.attached_planet = planet_index;
                 self.jumping = false;
-                self.height = ATTACHED_HEIGHT;
+                self.height = planet.radius + (SHIP_SIZE / 2.0);
                 self.rotation = (ship_y - planet.y).atan2(ship_x - planet.x);
             }
         }
@@ -126,11 +129,11 @@ fn main() {
     let mut gl = GlGraphics::new(opengl);
     let mut app = App {
         jumping: false,
-        height: ATTACHED_HEIGHT,
+        height: 75.0,
         rotation: 0.0,
-        planets: vec![Planet{x: 500.0, y: 500.0},
-                        Planet{x: 800.0, y: 300.0},
-                        Planet{x: 300.0, y: 200.0}],
+        planets: vec![Planet{x: 500.0, y: 500.0, radius: 50.0},
+                        Planet{x: 800.0, y: 300.0, radius: 35.0},
+                        Planet{x: 300.0, y: 200.0, radius: 100.0}],
         attached_planet: 0,
     };
 
