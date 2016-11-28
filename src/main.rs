337b205ade38c4eb72ea6cff4eaae3c45215f7a2
Christopher Armstrong <radix@twistedmatrix.com>
2016-11-28 13:31:09 -0600

    add closest-planet calculation & debug rendering

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use na::{Isometry2, Vector2};
 const SHIP_SIZE: f64 = 50.0;
 const SPEED: f64 = 5.0;
 const JUMP_SPEED: f64 = 3.0;
-const AIR_CONTROL_MOD: f64 = 0.1;
+const AIR_CONTROL_MOD: f64 = 50.0;
 
 #[derive(Debug)]
 struct Planet {
@@ -33,7 +33,8 @@ pub struct App {
     jumping: bool,
     height: f64,
     planets: Vec<Planet>,
-    attached_planet: usize
+    attached_planet: usize,
+    closest_planet_coords: (f64, f64),
 }
 
 impl App {
@@ -47,25 +48,26 @@ impl App {
     fn render(&mut self, mut gl: &mut GlGraphics, args: &RenderArgs) {
         use graphics::*;
 
-        // const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
         const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
         const BLUE:  [f32; 4] = [0.0, 0.0, 1.0, 1.0];
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
-        let square = rectangle::square(0.0, 0.0, SHIP_SIZE);
+        let square = graphics::rectangle::square(0.0, 0.0, SHIP_SIZE);
 
         let (x, y) = self.ship_position();
         gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
             clear(BLACK, gl);
-            let transform = c.transform.trans(x, y)
+            let ship_transform = c.transform.trans(x, y)
                                        .rot_rad(self.rotation)
-                                       .trans(-25.0, -25.0);
-            rectangle(RED, square, transform, gl);
+                                       .trans(-(SHIP_SIZE/2.0), -(SHIP_SIZE/2.0));
+            graphics::rectangle(RED, square, ship_transform, gl);
             for planet in &self.planets {
-                let circle = ellipse::circle(0.0, 0.0, planet.radius);
-                ellipse(BLUE, circle, c.transform.trans(planet.x, planet.y), gl);
+                let circle = graphics::ellipse::circle(0.0, 0.0, planet.radius);
+                graphics::ellipse(BLUE, circle, c.transform.trans(planet.x, planet.y), gl);
             }
+            let line = [x, y, self.closest_planet_coords.0, self.closest_planet_coords.1];
+            graphics::line(GREEN, 1.0, line, c.transform.trans(0.0, 0.0), gl);
         });
     }
 
@@ -77,22 +79,31 @@ impl App {
         }
         if self.jumping {
             self.height += JUMP_SPEED;
-            if left { self.rotation -= SPEED * AIR_CONTROL_MOD * args.dt}
-            if right { self.rotation += SPEED * AIR_CONTROL_MOD * args.dt}
+            if left { self.rotation -= SPEED * (AIR_CONTROL_MOD / self.height) * args.dt}
+            if right { self.rotation += SPEED * (AIR_CONTROL_MOD / self.height) * args.dt}
         }
 
-        // create ship ball
         let ship_ball = Ball::new(SHIP_SIZE);
         let (ship_x, ship_y) = self.ship_position();
         let ship_pos = Isometry2::new(Vector2::new(ship_x, ship_y), na::zero());
+        let mut closest_planet_distance = self.height;
         for (planet_index, planet) in (&self.planets).iter().enumerate() {
-            if planet_index == self.attached_planet {
-                continue;
-            }
             let planet_ball = Ball::new(planet.radius);
             let planet_pos = Isometry2::new(
                 Vector2::new(planet.x, planet.y),
                 na::zero());
+
+            // Check if this is the closest planet
+            let distance = query::distance(&ship_pos, &ship_ball, &planet_pos, &planet_ball);
+            if distance < closest_planet_distance {
+                closest_planet_distance = distance;
+                self.closest_planet_coords = (planet.x, planet.y);
+            }
+
+            if planet_index == self.attached_planet {
+                continue;
+            }
+            // Check if we've collided with the planet
             let collided = query::contact(&ship_pos, &ship_ball, &planet_pos, &planet_ball, 0.0);
             if let Some(_) = collided {
                 // We are landing on a new planet
@@ -135,6 +146,7 @@ fn main() {
                         Planet{x: 800.0, y: 300.0, radius: 35.0},
                         Planet{x: 300.0, y: 200.0, radius: 100.0}],
         attached_planet: 0,
+        closest_planet_coords: (500.0, 500.0),
     };
 
     let mut events = window.events().max_fps(1000);
