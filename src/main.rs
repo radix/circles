380d2513d51cd0725e9458dc35945d8f1ebdd676
Christopher Armstrong <radix@twistedmatrix.com>
2016-12-02 18:40:40 -0600

    implement an abstraction for "Space" and full procedural generation of the universe
    - safe API for "focus"ing on a point, which generates planets around it
    - safe API for fetching planets around the previously-focused point
    
    only unsafe api is get_planet_unsafe(idx: PlanetIndex), but as long as you
    don't construct your own PlanetIndexes it should be safe.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate nalgebra as na;
 extern crate rand;
 
 use std::f64::consts::PI;
+use std::collections::HashMap;
 use piston_window::*;
 use ncollide::query;
 use ncollide::shape::Ball;
@@ -51,8 +52,9 @@ pub struct App {
     jumping: bool,
     exit_speed: f64,
     height: f64,
-    planets: Vec<Planet>,
-    attached_planet: usize,
+    space: Space,
+    // planets: HashMap<(i32, i32), Vec<Planet>>, // planets are divided up into areas 1024x768 in size.
+    attached_planet: PlanetIndex,
     closest_planet_coords: Point, // redundant data, optimization
     // NES-style would be to make this a [(f64, f64); 3], so only three bullets can exist at once
     bullets: Vec<Bullet>,
@@ -60,8 +62,112 @@ pub struct App {
     camera_pos: Point,
 }
 
+#[derive(Clone, Copy, Debug)]
+pub struct PlanetIndex {
+    area: (i32, i32),
+    idx: usize,
+}
+
+#[derive(Debug)]
+pub struct Space {
+    planets: HashMap<(i32, i32), Vec<Planet>>,
+    current_point: Point,
+}
+
+impl Space {
+    fn new() -> Space {
+        let mut sp = Space {
+            planets: HashMap::new(),
+            current_point: Point::new(0.0, 0.0),
+        };
+        sp.realize();
+        sp
+    }
+
+    fn focus(&mut self, p: Point) {
+        self.current_point = p;
+        self.realize();
+    }
+
+    /// Return all nearby planets, but without generating any. This means that if you call this
+    /// with a point that hasn't been realize()d yet, it may panic.
+    fn get_nearby_planets(&self) -> Vec<(PlanetIndex, &Planet)> {
+        let mut planets = vec![];
+        for area in self.get_nearby_areas() {
+            if let Some(ps) = self.planets.get(&area) {
+                for (i, p) in ps.iter().enumerate() {
+                    planets.push((PlanetIndex {
+                                      area: area,
+                                      idx: i,
+                                  },
+                                  p));
+                }
+            } else {
+                panic!("Uninitialized area {:?} when in area {:?}",
+                       area,
+                       Space::get_area(self.current_point));
+            }
+        }
+        planets
+    }
+
+    fn get_planet_unsafe(&self, idx: PlanetIndex) -> &Planet {
+        &self.planets[&idx.area][idx.idx]
+    }
+
+    fn get_nearby_areas(&self) -> Vec<(i32, i32)> {
+        let area = Space::get_area(self.current_point);
+        let (x, y) = (area.0, area.1);
+        vec![(x - 1, y - 1),
+             (x - 1, y + 0),
+             (x - 1, y + 1),
+             (x + 0, y - 1),
+             (x + 0, y + 0),
+             (x + 0, y + 1),
+             (x + 1, y - 1),
+             (x + 1, y + 0),
+             (x + 1, y + 1)]
+    }
+
+    fn get_area(p: Point) -> (i32, i32) {
+        (p.x.floor() as i32 / 1024, p.y.floor() as i32 / 768)
+    }
+
+    /// Generate planets around the current center point
+    fn realize(&mut self) {
+        for area in self.get_nearby_areas() {
+            self.planets.entry(area).or_insert_with(|| Space::gen_planets(area));
+        }
+    }
+
+    /// Generate some planets for a given area. Each planet will have an appropriate x/y offset based
+    /// on the area given.
+    fn gen_planets(area: (i32, i32)) -> Vec<Planet> {
+        println!("Generating planets for {:?}", area);
+        let x_offset = 1024.0 * (area.0 as f64);
+        let y_offset = 768.0 * (area.1 as f64);
+        let range_width = Range::new(x_offset + 0.0, x_offset + 1024.0);
+        let range_height = Range::new(y_offset + 0.0, y_offset + 768.0);
+        let range_radius = Range::new(35.0, 100.0);
+        let mut rng = rand::thread_rng();
+        let range_num_planets = Range::new(1, 5);
+        let num_planets = range_num_planets.ind_sample(&mut rng);
+        (0..num_planets)
+            .map(|_| {
+                let x = range_width.ind_sample(&mut rng);
+                let y = range_height.ind_sample(&mut rng);
+                let radius = range_radius.ind_sample(&mut rng);
+                Planet {
+                    pos: Point::new(x, y),
+                    radius: radius,
+                }
+            })
+            .collect()
+    }
+}
+
 impl App {
-    fn render(&mut self, mut window: &mut PistonWindow, event: &Event) {
+    fn render(&self, mut window: &mut PistonWindow, event: &Event) {
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
         const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
         const BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
@@ -70,13 +176,15 @@ impl App {
         let square = rectangle::square(0.0, 0.0, SHIP_SIZE);
 
         window.draw_2d(event, |c, g| {
+            let planets = self.space.get_nearby_planets();
             let camera = c.transform.trans(-self.camera_pos.x, -self.camera_pos.y);
             clear(BLACK, g);
             let ship_transform = camera.trans(self.ship_pos.x, self.ship_pos.y)
                 .rot_rad(self.rotation)
                 .trans(-(SHIP_SIZE / 2.0), -(SHIP_SIZE / 2.0));
             rectangle(RED, square, ship_transform, g);
-            for planet in self.planets.iter() {
+
+            for (_, planet) in planets {
                 let circle = ellipse::circle(0.0, 0.0, planet.radius);
                 ellipse(BLUE, circle, camera.trans(planet.pos.x, planet.pos.y), g);
             }
@@ -90,7 +198,7 @@ impl App {
                                 self.closest_planet_coords.y];
             line(GREEN, 1.0, nearest_beam, camera.trans(0.0, 0.0), g);
 
-            let attached = &self.planets[self.attached_planet];
+            let attached = &self.space.get_planet_unsafe(self.attached_planet);
             let attached_beam = [self.ship_pos.x, self.ship_pos.y, attached.pos.x, attached.pos.y];
             line(BLUE, 1.0, attached_beam, camera.trans(0.0, 0.0), g);
         });
@@ -105,153 +213,146 @@ impl App {
               right: bool,
               shoot_target: Option<Point>,
               attach: bool) {
-        let attached_planet = &self.planets[self.attached_planet];
-        self.ship_pos.x = attached_planet.pos.x + (self.rotation.cos() * self.height);
-        self.ship_pos.y = attached_planet.pos.y + (self.rotation.sin() * self.height);
+        self.space.realize();
+        {
+            let attached_planet = self.space.get_planet_unsafe(self.attached_planet);
+            self.ship_pos.x = attached_planet.pos.x + (self.rotation.cos() * self.height);
+            self.ship_pos.y = attached_planet.pos.y + (self.rotation.sin() * self.height);
 
-        if let Some(target) = shoot_target {
-            if self.fire_cooldown <= 0.0 {
-                let angle = (target.y - self.ship_pos.y).atan2(target.x - self.ship_pos.x);
-                let bullet = Bullet {
-                    pos: self.ship_pos,
-                    dir: angle,
-                    speed: BULLET_SPEED,
-                };
-                self.bullets.push(bullet);
-                self.fire_cooldown = FIRE_COOLDOWN;
+            if let Some(target) = shoot_target {
+                if self.fire_cooldown <= 0.0 {
+                    let angle = (target.y - self.ship_pos.y).atan2(target.x - self.ship_pos.x);
+                    let bullet = Bullet {
+                        pos: self.ship_pos,
+                        dir: angle,
+                        speed: BULLET_SPEED,
+                    };
+                    self.bullets.push(bullet);
+                    self.fire_cooldown = FIRE_COOLDOWN;
+                } else {
+                    self.fire_cooldown -= args.dt;
+                }
+            }
+
+            let mut cull_bullets = vec![];
+            let mut cull_counter = 0;
+            for (idx, bullet) in self.bullets.iter_mut().enumerate() {
+                bullet.pos.x = bullet.pos.x + (bullet.speed * args.dt * bullet.dir.cos());
+                bullet.pos.y = bullet.pos.y + (bullet.speed * args.dt * bullet.dir.sin());
+                // kind of a dumb hack to determine when to cull bullets. It'd be better if we had the
+                // current view's bounding box (+ margin). or, alternatively, give each bullet a TTL.
+                if (bullet.pos.x - self.ship_pos.x).abs() > 5000.0 ||
+                   (bullet.pos.y - self.ship_pos.y).abs() > 5000.0 {
+                    // rejigger the index so when we delete the items they compensate for previous
+                    // deletions
+                    cull_bullets.push(idx - cull_counter);
+                    cull_counter += 1;
+                }
+            }
+
+            for cull_idx in cull_bullets {
+                self.bullets.remove(cull_idx);
+            }
+
+            if !self.jumping {
+                if left {
+                    self.rotation -= SPEED * args.dt;
+                }
+                if right {
+                    self.rotation += SPEED * args.dt;
+                }
+                if up {
+                    self.jumping = true;
+                    self.exit_speed = JUMP_SPEED;
+                }
             } else {
-                self.fire_cooldown -= args.dt;
+                self.height += self.exit_speed;
+                if left {
+                    self.rotation -= SPEED * (AIR_CONTROL_MOD / self.height) * args.dt
+                }
+                if right {
+                    self.rotation += SPEED * (AIR_CONTROL_MOD / self.height) * args.dt
+                }
+                if down {
+                    self.exit_speed -= ACCELERATION * args.dt;
+                }
+                if up {
+                    self.exit_speed += ACCELERATION * args.dt;
+                }
             }
-        }
 
-        let mut cull_bullets = vec![];
-        let mut cull_counter = 0;
-        for (idx, bullet) in self.bullets.iter_mut().enumerate() {
-            bullet.pos.x = bullet.pos.x + (bullet.speed * args.dt * bullet.dir.cos());
-            bullet.pos.y = bullet.pos.y + (bullet.speed * args.dt * bullet.dir.sin());
-            // kind of a dumb hack to determine when to cull bullets. It'd be better if we had the
-            // current view's bounding box (+ margin). or, alternatively, give each bullet a TTL.
-            if (bullet.pos.x - self.ship_pos.x).abs() > 5000.0 ||
-               (bullet.pos.y - self.ship_pos.y).abs() > 5000.0 {
-                // rejigger the index so when we delete the items they compensate for previous
-                // deletions
-                cull_bullets.push(idx - cull_counter);
-                cull_counter += 1;
-            }
-        }
+            let ship_ball = Ball::new(SHIP_SIZE / 2.0);
+            let ship_pos = Isometry2::new(Vector2::new(self.ship_pos.x, self.ship_pos.y),
+                                          na::zero());
+            let mut closest_planet_distance = self.height;
+            let mut closest_planet_idx: PlanetIndex = self.attached_planet;
+            let mut closest_planet = attached_planet;
 
-        for cull_idx in cull_bullets {
-            self.bullets.remove(cull_idx);
-        }
+            for (planet_index, planet) in self.space.get_nearby_planets() {
+                let planet_ball = Ball::new(planet.radius);
+                let planet_pos = Isometry2::new(Vector2::new(planet.pos.x, planet.pos.y),
+                                                na::zero());
 
-        if !self.jumping {
-            if left {
-                self.rotation -= SPEED * args.dt;
+                // Check if this is the closest planet
+                let distance = query::distance(&ship_pos, &ship_ball, &planet_pos, &planet_ball);
+                if distance < closest_planet_distance {
+                    closest_planet_distance = distance;
+                    closest_planet_idx = planet_index;
+                    closest_planet = planet;
+                    self.closest_planet_coords = Point::new(planet.pos.x, planet.pos.y);
+                }
+                let collided =
+                    query::contact(&ship_pos, &ship_ball, &planet_pos, &planet_ball, -1.0);
+                if let Some(_) = collided {
+                    // We are landing on a new planet
+                    self.attached_planet = planet_index;
+                    self.jumping = false;
+                    self.height = planet.radius + (SHIP_SIZE / 2.0);
+                    self.rotation = (self.ship_pos.y - planet.pos.y)
+                        .atan2(self.ship_pos.x - planet.pos.x);
+                    self.exit_speed = 0.0;
+                }
             }
-            if right {
-                self.rotation += SPEED * args.dt;
-            }
-            if up {
-                self.jumping = true;
-                self.exit_speed = JUMP_SPEED;
-            }
-        } else {
-            self.height += self.exit_speed;
-            if left {
-                self.rotation -= SPEED * (AIR_CONTROL_MOD / self.height) * args.dt
-            }
-            if right {
-                self.rotation += SPEED * (AIR_CONTROL_MOD / self.height) * args.dt
-            }
-            if down {
-                self.exit_speed -= ACCELERATION * args.dt;
-            }
-            if up {
-                self.exit_speed += ACCELERATION * args.dt;
-            }
-        }
-
-        let ship_ball = Ball::new(SHIP_SIZE / 2.0);
-        let ship_pos = Isometry2::new(Vector2::new(self.ship_pos.x, self.ship_pos.y), na::zero());
-        let mut closest_planet_distance = self.height;
-        let mut closest_planet_idx = self.attached_planet;
-        let mut closest_planet = &self.planets[self.attached_planet];
-
-        for (planet_index, planet) in self.planets.iter().enumerate() {
-            let planet_ball = Ball::new(planet.radius);
-            let planet_pos = Isometry2::new(Vector2::new(planet.pos.x, planet.pos.y), na::zero());
-
-            // Check if this is the closest planet
-            let distance = query::distance(&ship_pos, &ship_ball, &planet_pos, &planet_ball);
-            if distance < closest_planet_distance {
-                closest_planet_distance = distance;
-                closest_planet_idx = planet_index;
-                closest_planet = planet;
-                self.closest_planet_coords = Point::new(planet.pos.x, planet.pos.y);
-            }
-            let collided = query::contact(&ship_pos, &ship_ball, &planet_pos, &planet_ball, -1.0);
-            if let Some(_) = collided {
-                // We are landing on a new planet
-                self.attached_planet = planet_index;
-                self.jumping = false;
-                self.height = planet.radius + (SHIP_SIZE / 2.0);
-                self.rotation = (self.ship_pos.y - planet.pos.y)
-                    .atan2(self.ship_pos.x - planet.pos.x);
+            if attach {
+                // should we disallow attaching to the same planet? it basically allows player to
+                // hard-stop
+                self.attached_planet = closest_planet_idx;
                 self.exit_speed = 0.0;
+                self.rotation = (self.ship_pos.y - self.closest_planet_coords.y)
+                    .atan2(self.ship_pos.x - self.closest_planet_coords.x);
+                self.height = closest_planet_distance + closest_planet.radius + (SHIP_SIZE / 2.0);
+            }
+
+            if self.rotation > PI {
+                self.rotation -= 2.0 * PI
+            }
+            if self.rotation < -PI {
+                self.rotation += 2.0 * PI
+            }
+
+
+            let x_margin = (1.0 / 4.0) * view_size.width as f64;
+            let y_margin = (1.0 / 4.0) * view_size.height as f64;
+            let view_width_with_margin = view_size.width as f64 * (3.0 / 4.0);
+            let view_height_with_margin = view_size.height as f64 * (3.0 / 4.0);
+
+            if self.ship_pos.x > self.camera_pos.x + view_width_with_margin {
+                self.camera_pos.x += self.ship_pos.x - (self.camera_pos.x + view_width_with_margin);
+            } else if self.ship_pos.x < self.camera_pos.x + x_margin {
+                self.camera_pos.x += self.ship_pos.x - self.camera_pos.x - x_margin;
+            }
+            if self.ship_pos.y > self.camera_pos.y + view_height_with_margin {
+                self.camera_pos.y += self.ship_pos.y -
+                                     (self.camera_pos.y + view_height_with_margin);
+            } else if self.ship_pos.y < self.camera_pos.y + y_margin {
+                self.camera_pos.y += self.ship_pos.y - self.camera_pos.y - y_margin;
             }
         }
-        if attach {
-            // should we disallow attaching to the same planet? it basically allows player to
-            // hard-stop
-            self.attached_planet = closest_planet_idx;
-            self.exit_speed = 0.0;
-            self.rotation = (self.ship_pos.y - self.closest_planet_coords.y)
-                .atan2(self.ship_pos.x - self.closest_planet_coords.x);
-            self.height = closest_planet_distance + closest_planet.radius + (SHIP_SIZE / 2.0);
-        }
-
-        if self.rotation > PI {
-            self.rotation -= 2.0 * PI
-        }
-        if self.rotation < -PI {
-            self.rotation += 2.0 * PI
-        }
-
-
-        let x_margin = (1.0 / 4.0) * view_size.width as f64;
-        let y_margin = (1.0 / 4.0) * view_size.height as f64;
-        let view_width_with_margin = view_size.width as f64 * (3.0 / 4.0);
-        let view_height_with_margin = view_size.height as f64 * (3.0 / 4.0);
-
-        if self.ship_pos.x > self.camera_pos.x + view_width_with_margin {
-            self.camera_pos.x += self.ship_pos.x - (self.camera_pos.x + view_width_with_margin);
-        } else if self.ship_pos.x < self.camera_pos.x + x_margin {
-            self.camera_pos.x += self.ship_pos.x - self.camera_pos.x - x_margin;
-        }
-        if self.ship_pos.y > self.camera_pos.y + view_height_with_margin {
-            self.camera_pos.y += self.ship_pos.y - (self.camera_pos.y + view_height_with_margin);
-        } else if self.ship_pos.y < self.camera_pos.y + y_margin {
-            self.camera_pos.y += self.ship_pos.y - self.camera_pos.y - y_margin;
-        }
+        self.space.focus(self.ship_pos);
     }
 }
 
 fn main() {
-    let range_width = Range::new(0.0, 1024.0);
-    let range_height = Range::new(0.0, 768.0);
-    let range_radius = Range::new(35.0, 100.0);
-    let mut rng = rand::thread_rng();
-    let planets: Vec<Planet> = (0..3)
-        .map(|_| {
-            let x = range_width.ind_sample(&mut rng);
-            let y = range_height.ind_sample(&mut rng);
-            let radius = range_radius.ind_sample(&mut rng);
-            Planet {
-                pos: Point::new(x, y),
-                radius: radius,
-            }
-        })
-        .collect();
     let mut window: PistonWindow = WindowSettings::new("Circles", [1024, 768])
         .exit_on_esc(true)
         .vsync(false)
@@ -266,14 +367,17 @@ fn main() {
         fire_cooldown: 0.0,
         exit_speed: 0.0,
         ship_pos: Point::new(0.0, 0.0),
-        height: (planets[0]).radius,
+        height: 0.0,
         rotation: 0.0,
-        attached_planet: 0,
+        attached_planet: PlanetIndex {
+            area: (0, 0),
+            idx: 0,
+        },
         closest_planet_coords: Point::new(500.0, 500.0),
         bullets: vec![],
-        planets: planets,
+        space: Space::new(),
     };
-
+    app.height = app.space.get_planet_unsafe(app.attached_planet).radius;
     // probably move this into a struct
     let mut left = false;
     let mut right = false;
